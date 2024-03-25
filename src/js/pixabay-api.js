e3df4a639baeb40815943f31993fc58ad7860256
OleksandraStepanova <oleksandra.stepanova@gmail.com>
2024-03-25 09:10:34 +0100

    loader

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,7 +1,12 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
+const loader = document.querySelector('.loader');
+
 export function getPicture(query) {
+  loader.style.display = "block";
+
+
   const BASE_URL = 'https://pixabay.com/api/';
     const params = new URLSearchParams({
       key: '43034088-8742da6bcfba06a1e287112c2',
@@ -13,17 +18,20 @@ export function getPicture(query) {
     
     const url = `${BASE_URL}?${params}`;
 
-    return fetch(url).then(res => res.json()).then((data) => {
-        if (data.totalHits > 0) {
-            return data.hits;
-        }
-        else {
+  return fetch(url).then(res => res.json()).then((data) => {
+      loader.style.display = "none";
+      if (data.totalHits > 0) {        
+        return data.hits;
+      }
+      else {
         iziToast.show({
             message: "Sorry, there are no images matching your search query. Please try again!",
             timeout: 5000,
             position: "topRight",
             color: 'red'
         })
-    }
+      }
     });
+  
 }
+
--- src/main.js
+++ src/main.js
@@ -1,21 +1,35 @@
 import { getPicture } from './js/pixabay-api'
-import {photosTemplate} from './js/render-functions'
+import { photosTemplate } from './js/render-functions'
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 
 const formRef = document.querySelector(".search");
-const resultRef = document.querySelector(".result")
+const resultRef = document.querySelector(".result");
+const loader = document.querySelector('.loader');
+
+loader.style.display = "none";
 
 formRef.addEventListener('submit', createSearch);
 
 function createSearch(e) {
     e.preventDefault();
+    
+    resultRef.innerHTML = "";
 
     const inputValue = formRef.elements.input.value.trim();
-    
-    const photos = getPicture(inputValue).then((data => {
-        const markup = photosTemplate(data);
-        return resultRef.innerHTML= markup;
+
+    if (inputValue) {
+        const photos = getPicture(inputValue).then((data => {
+            const markup = photosTemplate(data);
+            resultRef.innerHTML = markup;
+            const gallery = new SimpleLightbox('.result li a', {});
+            gallery.refresh();
     }
     ));
+    }
+    
+    formRef.reset();
     
 }
